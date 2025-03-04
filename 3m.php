df96d05d803997255043fca892cd7b004a6292ab
vark1631 <vark1631@gmail.com>
2025-03-04 23:49:30 +0400

    Add files via upload

---
+++ 3m.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Евгений
+    </title>
+    <link rel="stylesheet" href="assets\css\3m.css" media="print" onload="this.media='all'">
+</head>
+<body>
+     <?php include ("header.php"); ?>
+
+    <hr class="hr-shadow">
+    <main>  <?php include ("about-act.php"); ?>
+        <div class="main-container">
+            <div class="main-char">
+          
+               <h2>Касатьев Петр</h2>
+               <p>Родился 7 января.</p>
+               <p>В 2014 году окончил СПбГАТИ (РГИСИ), специальность «Актерское искусство».
+               </p>
+               <p>В труппе театра с 2023 года</p>
+            
+
+            </div>
+        <div class="main-img">
+           <img src="assets\images\3m.jpg" alt="">
+        </div>
+    </main>
+ <?php include ("footer.php"); ?>
+ 
+</body>
+</html>
---
+++ 3w.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Марина
+    </title>
+    <link rel="stylesheet" href="assets\css\3w.css" media="print" onload="this.media='all'">
+</head>
+<body>
+    
+    <?php include ("header.php"); ?>
+    <hr class="hr-shadow">
+    
+    <main>  <?php include ("about-act.php"); ?>
+        <div class="main-container">
+            <div class="main-char">
+          
+               <h2>Вельмискина Анастасия</h2>
+               <p>Родилась 7 января.</p>
+               <p>В 2014 году окончил СПбГАТИ (РГИСИ), специальность «Актерское искусство».
+               </p>
+               <p>В труппе театра с 2023 года</p>
+            
+
+            </div>
+        <div class="main-img">
+           <img src="assets\images\3w.jpg" alt="">
+        </div>
+    </main>
+   <?php include ("footer.php"); ?>
+</body>
+</html>
